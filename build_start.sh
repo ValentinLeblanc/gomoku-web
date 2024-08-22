a6bac7248fe45b3fd55d0b1b07bb26c25e44309b
Valentin Leblanc <valentin.leblanc@becpg.fr>
2024-08-22 10:19:11 +0200

    Fix docker compose, add bash scripts

--- build_start.sh
+++ build_start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd ../gomoku-engine
 mvn clean install -DskipTests
-cd ../gomoku-web
+cd ../gomoku-web/src/main/nginx
+docker build -t gomoku-engine-http:1.0.0 .
+cd ../../../
 mvn clean install -DskipTests
 docker-compose up -d
 docker-compose logs -f
